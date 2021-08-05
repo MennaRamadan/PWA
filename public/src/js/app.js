7d6e4296a648d9a8f7e4e104c87df29dd42fe3e1
Mennatallah Ramadan <Mennatallah.Ramadan@itworx.com>
2021-08-05 16:36:22 +0200

    working on promise and callbacks

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -13,3 +13,31 @@ window.addEventListener('beforeinstallprompt', function(event){
     deferredPrompt = event;
     return false;
 })
+
+var promise = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        // resolve('this is excuted once the timer is done')
+        reject({code: 500, message: "An error occurred"})
+    }, 3000)
+})
+
+//For promise resolve
+// promise.then(function(text){
+//     console.log(text);
+// }, function(error){
+//     console.log(error.message);
+// })
+
+promise.then(function(text){
+        console.log(text);
+}).catch(function(error){
+    console.log(error.message);
+})
+// for promise reject
+
+
+//callback examples
+// setTimeout(function(){
+//     console.log('this is excuted once the timer is done')
+// }, 3000)
+console.log('This is excuted right after setTimeOut');
