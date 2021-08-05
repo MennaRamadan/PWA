ff1698f80d100ec37ec85f766247f646f0883504
Mennatallah Ramadan <Mennatallah.Ramadan@itworx.com>
2021-08-05 15:59:16 +0200

    bug fix

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,15 @@
+var deferredPrompt;
+
 //This mean that browser will excute the code if it support serviceworker
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('/sw.js').then(function(){
         console.log('Serviceworker registered');
     });
 }
+
+window.addEventListener('beforeinstallprompt', function(event){
+    console.log('beforeinstallprompt fired')
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+})
--- public/sw.js
+++ public/sw.js
@@ -5,4 +5,8 @@ self.addEventListener('activate', function(event){
     console.log('[Service worker] Activating service worker ...', event);
     return self.clients.claim();
 });
+self.addEventListener('fetch', function(event){
+    console.log('[Service worker] Fetching Something ...', event);
+    event.respondWith(fetch(event.request));
+});
 
