f42d176cff93e10f4821c3e4f261e114a9100595
Mennatallah Ramadan <Mennatallah.Ramadan@itworx.com>
2021-08-08 14:54:38 +0200

    working with promise and fetch

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,9 +1,15 @@
 var deferredPrompt;
 
+if(!window.Promise){
+    window.Promise = Promise
+}
+
 //This mean that browser will excute the code if it support serviceworker
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('/sw.js').then(function(){
         console.log('Serviceworker registered');
+    }).catch(function(err){
+        console.log(err)
     });
 }
 
@@ -19,6 +25,59 @@ var promise = new Promise(function(resolve, reject){
         // resolve('this is excuted once the timer is done')
         reject({code: 500, message: "An error occurred"})
     }, 3000)
+});
+
+//So what to use XML or fetch 
+//We will use fetch as  fetch is async which will work with service workers but XML is sync 
+var xhr = XMLHttpRequest();
+xhr.open('GET', 'https://httpbin.org/ip');
+xhr.responseType = 'json';
+
+xhr.onload = function(){
+    console.log(xhr.response);
+}
+
+xhr.onerror = function(){
+    console.log('XML Error');
+}
+
+xhr.send();
+
+//Example how to use fetch to get data
+fetch('https://httpbin.org/ip')
+.then(function(response){
+    console.log(response);
+    //we need to parse the response 
+    return response.json();
+}).then(function(data){
+    console.log(data);
+}).catch(function(error){
+    console.log(error);
+})
+
+
+//Example how to use fetch to post data
+fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json',
+        'Accept' : 'application/json',
+    },
+    //this means that the response will include cors headers
+    //we choose the mode to be cors to be able to access the data from javascript
+    mode: 'cors',
+    body: JSON.stringify({
+        message: 'Does this work!'
+    })
+})
+.then(function(response){
+    console.log(response);
+    //we need to parse the response 
+    return response.json();
+}).then(function(data){
+    console.log(data);
+}).catch(function(error){
+    console.log(error);
 })
 
 //For promise resolve
