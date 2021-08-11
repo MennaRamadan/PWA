db4f5adfce7e2edd020269e69dc528d5f6fce846
Mennatallah Ramadan <Mennatallah.Ramadan@itworx.com>
2021-08-11 15:55:48 +0200

    working with static cache, dynamic cache

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,38 @@
+var CACHE_STATIC_NAME = 'static-v4';
+var CACH_DYNAMIC_NAME = 'dynamic-v2'
+
 self.addEventListener('install', function(event){
-    event.waitUntill(caches.open('static').then(function(cache){
-        console.log('[Service workers] Precaching files')
-        cache.add('/');
-        cache.add('/index.html');
-        cache.add('/src/js/app.js');
+    event.waitUntill(caches.open(CACHE_STATIC_NAME).then(function(cache){  
+        console.log('[Service workers] Precaching files') 
+        cache.addAll([
+            '/',
+            '/index.html',
+            '/src/js/app.js',
+            '/src/js/feed.js',
+            '/src/js/promise.js',
+            '/src/js/fetch.js',
+            '/src/js/material.min.js',
+            '/src/css/app.css',
+            '/src/css/feed.css',
+            '/src/images/main-image.jpg',
+            'https://fonts.googleapis.com/css?family=Roboto:400,700',
+            'https://fonts.googleapis.com/icon?family=Material+Icons',
+            'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
     }));
 });
 self.addEventListener('activate', function(event){
+    event.waitUntill(
+        caches.keys().then(function(keyList){
+            //return this function when all return
+            return Promise.all(keyList.map(function(key){
+                if(key !== CACHE_STATIC_NAME && key !== CACH_DYNAMIC_NAME){
+                    console.log('[Service worker .. removing old case]');
+                    return caches.delete(key);
+                }
+            }))
+        })
+    )
     return self.clients.claim();
 });
 self.addEventListener('fetch', function(event){
@@ -17,7 +43,14 @@ self.addEventListener('fetch', function(event){
                 return response;
             }else{
                 //cont with server request
-                return fetch(event.request);
+                return fetch(event.request).then(function(res){
+                    caches.open(CACH_DYNAMIC_NAME).then(function(cache){
+                        cache.put(event.request.url, res.clone())
+                        return res;
+                    })
+                }).catch(function(){
+
+                });
             }
         })
     );
